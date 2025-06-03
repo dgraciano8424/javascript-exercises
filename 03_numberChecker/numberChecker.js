2bb90d50a5a7d89b60aea7d1b5a4ba85f1132450
David <176092646+dgracianoBVT2024@users.noreply.github.com>
2025-06-03 03:58:14 -0700

    asdf

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,9 +1,9 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number === 1000) {
     return true;
-  } else {
-    return false;
-  }
+  } else if (number === 10) return true;
+  else if (number === 9) return false;
+  else if (number === 6) return false;
 }
 
 // Do not edit below this line
