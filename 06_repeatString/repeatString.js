391fc940d2f9eea536fa52c3f4c9b2d9e05bc2de
David <176092646+dgracianoBVT2024@users.noreply.github.com>
2025-06-09 00:26:03 -0700

    123

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (String, num) {
+  return repeatString(3);
 };
 
+repeatString("hey", 3);
+// function repeatString(str, times) {
+//   return str.repeat(times);
+// }
 // Do not edit below this line
 module.exports = repeatString;
